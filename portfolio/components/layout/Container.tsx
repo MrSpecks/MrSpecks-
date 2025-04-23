3e5ceb1974eed891131d6e69297cf6a6b82c0810
Marspexx <cybernetkae@gmail.com>
2025-04-23 11:59:36 +0200

    Added content to about.tsx and created container.tsx

---
+++ portfolio/components/layout/Container.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+interface ContainerProps {
+  children: React.ReactNode;
+}
+
+export default function Container({ children }: ContainerProps) {
+  return (
+    <div className="px-4 sm:px-6 md:px-8 py-8 max-w-screen-lg mx-auto">
+      {children}
+    </div>
+  );
+}
