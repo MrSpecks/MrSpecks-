064e5304e3092cddf6e213f6db1a38313f35cd61
Marspexx <cybernetkae@gmail.com>
2025-04-24 19:27:16 +0200

    Configure ESLint to be ignored during production builds and add eslint.config.mjs

--- portfolio/next.config.js
+++ portfolio/next.config.js
@@ -1,11 +1,17 @@
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
     swcMinify: true,
     images: {
       domains: ['your-image-host.com'], // Add trusted image sources if using next/image
-      unoptimized: true //helps with deployment issues
-    }
+      unoptimized: true, //helps with deployment issues
+    },
+    eslint: {
+      // Warning: This allows production builds to successfully complete even if
+      // your project has ESLint errors.
+      ignoreDuringBuilds: true,
+    },
   };
   
   module.exports = nextConfig;
