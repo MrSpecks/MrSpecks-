2619649e576301f0a363feb2019aca0ecc4eec6a
Marspexx <cybernetkae@gmail.com>
2025-04-24 13:13:53 +0200

    buttons import corrections

--- portfolio/components/ProjectCard.tsx
+++ portfolio/components/ProjectCard.tsx
@@ -1,7 +1,7 @@
 // components/cards/ProjectCard.tsx
 import React from "react";
 import Link from "next/link";
-import LinkButton from "./Buttons/LinkButton"
+import LinkButton from "./buttons/LinkButton"
 
 export type Project = {
 id: number;
