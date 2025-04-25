d240b79f67d4cc31ff0782ab3453cc688299621b
Marspexx <cybernetkae@gmail.com>
2025-04-25 02:04:19 +0200

    added scroll-to-top button, page transition animations, and tech stack showcase

--- portfolio/pages/_app.tsx
+++ portfolio/pages/_app.tsx
@@ -1,12 +1,25 @@
-import React from 'react';
+import React from "react";
+import type { AppProps } from "next/app";
+import { AnimatePresence, motion } from "framer-motion";
 import Layout from "../components/Layout";
-import { AppProps } from 'next/app';
-import '../styles/globals.css';
+import ScrollToTop from "../components/ScrollToTop";
+import "../styles/globals.css";
 
-export default function App({Component, pageProps}: AppProps) {
-    return (
+export default function App({ Component, pageProps, router }: AppProps) {
+  return (
     <Layout>
-        <Component {...pageProps} />
+      <AnimatePresence mode="wait">
+        <motion.div
+          key={router.route}
+          initial={{ opacity: 0, y: 16 }}
+          animate={{ opacity: 1, y: 0 }}
+          exit={{ opacity: 0, y: -16 }}
+          transition={{ duration: 0.25, ease: "easeInOut" }}
+        >
+          <Component {...pageProps} />
+        </motion.div>
+      </AnimatePresence>
+      <ScrollToTop />
     </Layout>
-    )
+  );
 }
