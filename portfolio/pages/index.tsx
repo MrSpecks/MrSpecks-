b315ad31f5d7769e6c42428b0e520f23912f412b
Marspexx <cybernetkae@gmail.com>
2025-04-26 13:52:28 +0200

    Update index.tsx

--- portfolio/pages/index.tsx
+++ portfolio/pages/index.tsx
@@ -99,7 +99,7 @@ export default function HomePage() {
       {/* Featured Projects */}
       <section className="mt-16">
         <h2 className="text-xl font-semibold mb-4 text-center">Featured Projects</h2>
-        <div className="grid grid-cols-4 sm:grid-cols-2 lg:grid-cols-4 gap-6">
+        <div className="grid gap-6">
         <Carousel items={featured} />
         </div>
         </section>
